5e49c01fd9b8d9d79c71dce1b9a292890f85c175
Murali <makkalamurali2001@gmail.com>
2023-01-01 15:28:54 +0530

    hello

---
+++ src/main/java/com/murali/train/Entity/first.java
@@ -0,0 +1,9 @@
+package com.murali.train.Entity;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello murali");
+    }
+
+
+}
---
+++ src/main/java/com/murali/train/Executor/first/Executor.java
@@ -0,0 +1,7 @@
+package com.murali.train.Executor.first;
+
+public class Executor {
+    public static void main(String[] args) {
+        System.out.println("hello !..");
+    }
+}
