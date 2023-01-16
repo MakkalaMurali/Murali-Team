2ff6726f994ae8daaf126ecad69ce4afd836f23d
Murali <makkalamurali2001@gmail.com>
2023-01-16 16:06:36 +0530

    hiiiiii

--- src/main/java/com/murali/train/Entity/first.java
+++ src/main/java/com/murali/train/Entity/first.java
@@ -1,7 +1,13 @@
 package com.murali.train.Entity;
 
+import java.io.File;
+import java.io.IOException;
+
 public class first {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello Murali");
+        File f =new File("D:\\filesdemo\\resume.txt");
+        System.out.println(f.createNewFile());
+//        System.out.println(f.delete());
     }
 }
